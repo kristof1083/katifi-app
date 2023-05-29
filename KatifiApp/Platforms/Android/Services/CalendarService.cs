26a819148ad6288ff1aba1e48cbba52e558f9acb
lytwp8 <lytwp8@pte.hu>
2023-05-29 13:29:31 +0200

    Correct local Calendar date handling

--- KatifiApp/Platforms/Android/Services/CalendarService.cs
+++ KatifiApp/Platforms/Android/Services/CalendarService.cs
@@ -16,12 +16,20 @@ namespace KatifiApp.Platforms.Android.Services
 
             string description = $"Organizer: {varevent.Organizer}.{Environment.NewLine}Fee: {varevent.Fee}.{Environment.NewLine}Max participants: {varevent.MaxParticipant}.";
 
+            double startvalue = varevent.Start.ToUniversalTime().Subtract(
+                        new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+            double endvalue = varevent.End.ToUniversalTime().Subtract(
+                        new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+
+
             intent.PutExtra(CalendarContract.Events.InterfaceConsts.Title, varevent.Name);
-            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Dtstart, varevent.Start.ToString());
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Dtstart, startvalue);
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.Dtend, endvalue);
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.CalendarTimeZone, "Europe/Budapest");
             intent.PutExtra(CalendarContract.Events.InterfaceConsts.Organizer, varevent.Organizer);
             intent.PutExtra(CalendarContract.Events.InterfaceConsts.EventLocation, varevent.Address.ToString());
             intent.PutExtra(CalendarContract.Events.InterfaceConsts.Description, description);
-            intent.PutExtra(CalendarContract.Events.InterfaceConsts.AllDay, true);
+            intent.PutExtra(CalendarContract.Events.InterfaceConsts.AllDay, false);
             
             if(intent.ResolveActivity(Platform.CurrentActivity.PackageManager) != null)
             {
